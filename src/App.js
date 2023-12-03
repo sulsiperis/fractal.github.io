e38caca98f22916eb9a31bbddf58f5d3ccaf2d93
sasu.wunu@gmail.com <Sulsiperis>
2023-12-03 02:51:16 +0200

    -Changed size and style of buttons -Added "toggle tooltips button" -Removed tooltips on hover -Changed tooltip color -First time setting start position doesn't erase canvas anymore -Changed zoom resizing method into wrapping div resizing (still buggy)
    Signed-off-by: sasu.wunu@gmail.com <Sulsiperis>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Triangle } from './func/triangle_class';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Overlay from 'react-bootstrap//Overlay';
 import Tooltip from 'react-bootstrap/Tooltip';
 
 function App() {
@@ -15,27 +15,37 @@ function App() {
   const [clickCount, setClickCount] = React.useState(0)
   const [lastPos, setLastPos] = React.useState()
   const [maxTriSize, setMaxTriSize] = React.useState(30)
-  const [showHelp, setShowHelp] = React.useState(false);
+  const [showHelp, setShowHelp] = React.useState(false)
+  const [showToolTips, setShowToolTips] = React.useState(false)
 
-  const handleHelpClose = () => setShowHelp(false);
-  const handleHelpShow = () => setShowHelp(true);
+
+  const handleHelpClose = () => setShowHelp(false)
+  const handleHelpShow = () => setShowHelp(true)
 
   
   const cnv = React.useRef()
+  const wrapper = React.useRef()
+  const btn_tri = React.useRef()
+  const btn_dots = React.useRef()
+  const btn_help = React.useRef()
+  const btn_zoomin = React.useRef()
+  const btn_zoomout = React.useRef()
+  const btn_clear = React.useRef()
+  const slider_dots = React.useRef()
+  const slider_tri = React.useRef()
 
+
+  //mouse coord event
   React.useEffect(() => {
-    //cnv.current.addEventListener('mousemove', function(evt) {
+    
     cnv.current.addEventListener('click', function(evt) {    
         const mousePos = getMousePos(cnv.current, evt);
         //console.log(startPos)
         if (!working) {          
           const tempPos = {x: mousePos.x, y: mousePos.y}
           setTempStartPos(tempPos)
-        }
-        //!working && setStartPos({x: mousePos.x, y: mousePos.y})
-        //!working && console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
-    }, false);    
-
+        }        
+    }, false);
     setMaxTriSize(cnv.current.width/2 - (cnv.current.width/100*3))
     /* cnv.current.addEventListener('mousemove', function(evt) {    
       const mousePos = getMousePos(cnv.current, evt);
@@ -43,15 +53,14 @@ function App() {
     }, false); */
     
   }, [])
+
   //set canvas size due to windows resize
   React.useEffect(() => {
-    window.addEventListener("resize", function() {
-      
+   /*  window.addEventListener("resize", function() {      
       resizeCanvas()
-
       //console.log(canv.height)
-    })
-    resizeCanvas()
+    }) */
+    resizeCanvas(true)
   }, [])
 
   //console.log(startPos)
@@ -72,11 +81,17 @@ function App() {
           setClickCount(0)
           //triangle2(false)
           drawPoint(getZoomedStartPos(tempStartPos, cnv))
-        } 
-      } else {
-        clear()       
+        } else {
+          
+        }
+      } else {        
+        //clickCount<1 && clear()
+        if (startPos) {
+          clear()
+          setClickCount(0)
+        }        
         setStartPos(getZoomedStartPos(tempStartPos, cnv))        
-        setClickCount(0)
+        
         //triangle2(false)
         drawPoint(getZoomedStartPos(tempStartPos, cnv))
       }
@@ -86,6 +101,8 @@ function App() {
   React.useEffect(() => {
     triangle2()
   }, [])
+
+
   function getZoomedStartPos(rawStartPos, canv) {
     let newStartPos
       const widthStr = canv.current.style.width
@@ -103,25 +120,41 @@ function App() {
 
   }
 
-  function resizeCanvas() {
+  function resizeCanvas(initial=false) {
     const canv = document.getElementById('my-canvas')
-      const new_width = window.innerWidth / 100 * 40;
-      const new_height = window.innerWidth / 100 * 40;
-      if (new_width <= 630 && new_width >= 240)  {
-        
-        canv.width = new_width
-        canv.height = new_height
-      // setWIndowWidth(window.innerWidth)
-      } else {
-        if (new_width < 240) {
-          canv.width = 240
-          canv.height = 240
-        } else if (new_width > 630) {
-          canv.width = 630
-          canv.height = 630
-        }
+    const vw = document.documentElement.clientWidth
+    const vh = document.documentElement.clientHeight
+    let new_width, new_height
+    if (vw > vh) {
+      new_width = vh / 100 * 70;
+      new_height = vh / 100 * 70;  
+    } else {
+      new_width = vw / 100 * 70;
+      new_height = vw / 100 * 70;  
+    }
+
+    console.log("viewport: ", vw, vh)
+    console.log(new_width, new_height)
+
+    //const new_width = window.innerWidth / 100 * 40;
+    //const new_height = window.innerWidth / 100 * 40;
+    if (new_width <= 630 && new_width >= 240)  {
+
+      canv.width = new_width
+      canv.height = new_height
+
+    // setWIndowWidth(window.innerWidth)
+    } else {
+      if (new_width < 240) {
+        canv.width = 240
+        canv.height = 240        
+      } else if (new_width > 630) {
+        canv.width = 630
+        canv.height = 630
       }
-      setMaxTriSize(canv.width/2 - (canv.width/100*3))
+    }
+    
+    setMaxTriSize(canv.width/2 - (canv.width/100*3))
   }
 
   function drawPoint(point, color="white", size=1) {
@@ -240,7 +273,17 @@ function App() {
     //console.log("finnished")
   }
 
- function clear() {
+  function handleClearClick() {
+    if (window.confirm('Are you sure you want to clear canvas?')) {
+      clear(); 
+      zoom("reset"); 
+      setClickCount(0); 
+      setStartPos()
+    }
+    
+  }
+
+  function clear() {
     const ctx = cnv.current.getContext("2d");
     ctx.clearRect(0, 0, cnv.current.width, cnv.current.height);
     setLastPos()    
@@ -254,6 +297,47 @@ function App() {
     
   }
   //resizes canvas. @opt = string with possible values: "in", "out", "reset"
+
+  function zoom2(opt="in", minSize=280, maxSize=1000) { 
+    const widthStr = wrapper.current.style.width    
+    const styleWidth = widthStr!==""?Number(widthStr.slice(0, widthStr.length - 2)):cnv.current.width+40
+    console.log(styleWidth)
+    switch (opt) {
+      case "in": {
+        if (styleWidth < maxSize) {
+          wrapper.current.style.width = (styleWidth + 100) + 'px';
+          wrapper.current.style.height = (styleWidth + 100 + 40) + 'px';
+          
+        } else {
+          alert("Max zoom reached!")
+        }
+        break;
+      }
+      case "out": {
+        if (styleWidth > minSize) {
+          wrapper.current.style.width = (styleWidth - 100) + 'px';
+          wrapper.current.style.height = (styleWidth - 100 +40) + 'px';
+          
+        } else {
+          alert("Min zoom reached!")
+        }
+        break;
+      }
+      case "reset": {
+        wrapper.current.style.width = "";
+        wrapper.current.style.height = "";
+        break;
+      }
+      default: {
+        wrapper.current.style.width = "";
+        wrapper.current.style.height = "";
+        break;
+      }
+    }
+    //console.log(cnv.current.style.width, cnv.current.style.height)
+    
+  }
+
   function zoom(opt="in", minSize=240, maxSize=1000) { 
     const widthStr = cnv.current.style.width
     const styleWidth = widthStr!==""?Number(widthStr.slice(0, widthStr.length - 2)):cnv.current.width
@@ -293,138 +377,105 @@ function App() {
     
   }
 
-  const renderTooltip = (text, id) => (
-    <Tooltip id={"button-tooltip" + id}>
-      {text}
-    </Tooltip>
-  );
+  function tooltip(target, placement="top", text="") {
+    return (
+      <Overlay target={target.current} show={showToolTips} placement={placement}>
+        {(props) => (
+          <Tooltip id="overlay-example" {...props}>
+            {text}
+          </Tooltip>
+        )}
+      </Overlay>
+    )
+  }
 
   return (
-    <div className='main'>
-      <Offcanvas show={showHelp} onHide={handleHelpClose} placement='start' >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>About</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <div className='help-content'>
-              <p>
-              Based on Sierpinski's triangle (Chaos game).<br/>
-              Place a start point by mouse click somewhere inside black rectangle (preferably within the borders of the triangle). Now press the "Δ" button to mark triangle boundaries or vertices. Press the "." button to start placing dots. By sliding left and right change the number of dots to be drawn. You can also resize the triangle with the slider on the left and then redraw it by pressing "Δ". Use "+" and "-" buttons to zoom in/out and "X" button to clear all canvas and reset zoom.
-              </p>
-              <p><strong>Explanation:</strong></p>
-              <p className='txt12'>Algorithm draws a straight imaginary line from your given start point to one of three randomly chosen vertices of triangle. Then it draws a dot in the middle of this line. The next imaginary line goes from this new point and new dot half way and so on. Every time vertices are chosen randomly and the same vertex can be chosen any number times in a row. After drawing many points appears triangle fractal. It looks always the same even if a start point placed outside the boundaries of triangle. 
-              You can try this by placing dots one by one or take a pencil, ruler and draw it on a paper. The more dots - the clearer fractal drawing. </p>
-          </div>
-          <p style={{display: "flex", justifyContent: "center"}}><strong>Made by Sulsiperis 2023</strong></p>
-        </Offcanvas.Body>
-      </Offcanvas>
-      <div className='wrapper'>
-        <div className='dot-toolbar'>
+    <div className='main-wrapper' data-bs-theme='light'>
 
-        <OverlayTrigger
-              placement="bottom"
-              /* delay={{ show: 250, hide: 400 }} */
-              overlay={renderTooltip("Number of dots", 4)}
-            >
-              <input 
-                type='range' 
-                min={1} max={999} 
-                className='start-range' 
-                name='dotNumber' 
-                disabled={working?true:false} 
-                value={dotNum} 
-                onChange={handleDotNumChange} 
-              />
-            </OverlayTrigger>
+      {tooltip(btn_tri, "right", "Draw triangle or vertices")}
+      {tooltip(btn_dots, "bottom", "Start drawing dots")}
+      {tooltip(btn_zoomin, "top", "Zoom in")}
+      {tooltip(btn_zoomout, "bottom", "Zoom out")}
+      {tooltip(btn_clear, "left", "Clear canvas and reset zoom")}
+      {tooltip(btn_help, "top", "Help / about")}
+      {tooltip(slider_dots, "left", "Amount of dots")}
+      {tooltip(slider_tri, "top", "Size of triangle")}
 
-          
-          <label htmlFor="dotNumber">{dotNum}</label>
-          <OverlayTrigger
-            placement="left"
-            /* delay={{ show: 250, hide: 400 }} */
-            overlay={renderTooltip("Start drawing dots", 1)}
-          >
+
+
+      <div className='main'>
+        <Offcanvas show={showHelp} onHide={handleHelpClose} placement='start' >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>About</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <div className='help-content'>
+                <p>
+                Based on Sierpinski's triangle (Chaos game).<br/>
+                Press "..." button to toggle tooltips. <br/>
+                Place a start point by mouse click somewhere inside black rectangle (preferably within the borders of the triangle). Now press the "Δ" button to mark triangle boundaries or vertices. Press the "." button to start placing dots. By sliding left and right change the number of dots to be drawn. You can also resize the triangle with the slider on the left and then redraw it by pressing "Δ". Use "+" and "-" buttons to zoom in/out and "X" button to clear all canvas and reset zoom.
+                </p>
+                <p><strong>Explanation:</strong></p>
+                <p className='txt12'>Algorithm draws a straight imaginary line from your given start point to one of three randomly chosen vertices of triangle. Then it draws a dot in the middle of this line. The next imaginary line goes from this new point and new dot half way and so on. Every time vertices are chosen randomly and the same vertex can be chosen any number times in a row. After drawing many points appears triangle fractal. It looks always the same even if a start point placed outside the boundaries of triangle. 
+                You can try this by placing dots one by one or take a pencil, ruler and draw it on a paper. The more dots - the clearer fractal drawing. </p>
+            </div>
+            <p style={{display: "flex", justifyContent: "center"}}><strong>Made by Sulsiperis 2023</strong></p>
+          </Offcanvas.Body>
+        </Offcanvas>
+        <div className='wrapper' ref={wrapper}>
+          <div className='dot-toolbar'>
+            <input 
+              type='range' 
+              min={1} max={999} 
+              className='start-range' 
+              name='dotNumber' 
+              disabled={working?true:false} 
+              value={dotNum} 
+              onChange={handleDotNumChange}
+              ref={slider_dots}
+            />
+            <label htmlFor="dotNumber">{dotNum}</label>            
             <button 
+              ref={btn_dots}
               disabled={working?true:false} 
               className='start-btn' 
               onClick={() => dots2(dotNum)} 
               style={{background: 'transparent', 
               border: "1px solid #000"  }}
             ><strong>·</strong></button>            
-          </OverlayTrigger>
-
-
-
-          
-        </div>
-        <div className='tri-params-wrapper'>
-          <div className='tri-slider'>
-          <OverlayTrigger
-              placement="right"
-              /* delay={{ show: 250, hide: 400 }} */
-              overlay={renderTooltip("Size of triangle", 3)}
-            >
-              <input 
-              type='range' 
-              orient='vertical' 
-              min={30} max={maxTriSize} 
-              className='start-range-vertical' 
-              name='triangleSize' 
-              disabled={working?true:false} 
-              value={triangleSize} 
-              onChange={handleTriSizeChange} 
-            />
-            </OverlayTrigger>
-            
-            <label htmlFor="triangleSize">{triangleSize}</label>
-            
-            <OverlayTrigger
-              placement="right"
-              /* delay={{ show: 250, hide: 400 }} */
-              overlay={renderTooltip("Draw triangle", 2)}
-            >
-              <button disabled={working?true:false} className='start-btn' onClick={() => triangle2()} ><strong>Δ</strong></button>
-            </OverlayTrigger>
-
-
-            
           </div>
-          <canvas className='my-canvas' id="my-canvas" ref={cnv} ></canvas>
+          <div className='tri-params-wrapper'>
+            <div className='tri-toolbar'>
+              <input
+                ref={slider_tri} 
+                type='range' 
+                orient='vertical' 
+                min={30} max={maxTriSize} 
+                className='start-range-vertical' 
+                name='triangleSize' 
+                disabled={working?true:false} 
+                value={triangleSize} 
+                onChange={handleTriSizeChange} 
+              />
+              <label htmlFor="triangleSize">{triangleSize}</label>
+              <button disabled={working?true:false} ref={btn_tri} className='start-btn' onClick={() => triangle2()} ><strong>Δ</strong></button>
+            </div>
+
+            <canvas className='my-canvas' id="my-canvas" ref={cnv}></canvas>
+
+          </div>
+          <div className='footer'>
+            <div className='footer-buttons'>
+              <button ref={btn_help} className='start-btn help' onClick={() => {handleHelpShow()}}><strong>?</strong></button>
+              <button className='start-btn help' onClick={() => {setShowToolTips(!showToolTips)}}><strong>...</strong></button>
+              <button ref={btn_zoomout} disabled={working?true:false} className='start-btn' onClick={() => {zoom2("out")}}><strong>-</strong></button>
+              <button ref={btn_zoomin} disabled={working?true:false} className='start-btn' onClick={() => {zoom2("in")}}><strong>+</strong></button>
+            </div>
+            <button ref={btn_clear} disabled={working?true:false} className='start-btn back-red' onClick={() => {handleClearClick()}}><strong>x</strong></button>
+          </div>
         </div>
-        <div className='footer'>
-          <OverlayTrigger
-            placement="left"
-            /* delay={{ show: 250, hide: 400 }} */
-            overlay={renderTooltip("Help / About", 5)}
-          >
-            <button className='start-btn help' onClick={() => {handleHelpShow()}}><strong>?</strong></button>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="left"
-            /* delay={{ show: 250, hide: 400 }} */
-            overlay={renderTooltip("Zoom out", 6)}
-          >
-            <button disabled={working?true:false} className='start-btn' onClick={() => {zoom("out")}}><strong>-</strong></button>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="left"
-            /* delay={{ show: 250, hide: 400 }} */
-            overlay={renderTooltip("Zoom in", 7)}
-          >
-            <button disabled={working?true:false} className='start-btn' onClick={() => {zoom("in")}}><strong>+</strong></button>
-          </OverlayTrigger>
-          <OverlayTrigger
-            placement="left"
-            /* delay={{ show: 250, hide: 400 }} */
-            overlay={renderTooltip("Clear canvas and reset zoom", 8)}
-            
-          >
-            <button disabled={working?true:false} className='start-btn back-red' onClick={() => {clear(); zoom("reset"); setClickCount(0); setStartPos()}}><strong>x</strong></button>
-          </OverlayTrigger>
-          
-        </div>
+        
       </div>
-      
     </div>
   );
 }
